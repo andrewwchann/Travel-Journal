c972055412316388a60c535476297bbe8507d951
ANDREW CHAN <MY_NAME@example.com>
2023-06-19 18:05:58 -0600

    Small database in the form of an object so it can be used as a prop

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        id: 1,
+        title: "Cousin Family Trip",
+        location: "Vancouver",
+        googleMapsURL: "https://www.google.com/maps/dir//Downtown+Vancouver,+Vancouver,+BC/@49.2867586,-123.1217279,14z/data=!4m9!4m8!1m0!1m5!1m1!1s0x5486717f41ba2fb1:0xc6952794560a44aa!2m2!1d-123.1170744!2d49.281954!3e0?entry=ttu",
+        date: "June 24, 2023",
+        description: "This was our first cousin family trip! A small trip that had a lot of eating, exploring, and relaxing. Most of it was finding places to eat and just enjoying each others presence for the week. We usually don't get to see each other often."
+    },
+
+    {
+        id: 2,
+        title: "Moon Palace",
+        location: "Cancun, Mexico",
+        googleMapsURL: "https://www.google.com/maps/dir//Moon+Palace+Cancun+-+All+Inclusive+Resort,+Cancun+-+Chetumal+Km+340,+Riviera+Maya,+77500+Canc%C3%BAn,+Q.R.,+Mexico/@20.9888457,-86.8990869,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x8f4c285c52112ea7:0xa7daf8531476b25e!2m2!1d-86.8290465!2d20.98886?hl=en-US&entry=ttu",
+        date: "Dec 30, 2023",
+        description: "Moon Palace is a resort in Cancun with many activities. They are right along the ocean so theres many ocean activites, numerous pools, drinks, food, etc. I spent 2 weeks here and it was nothing short of amazing."
+    },
+
+    {
+        id: 3,
+        title: "Grad Trip",
+        location: "Brag Creek",
+        googleMapsURL: "https://www.google.com/maps/dir//Bragg+Creek,+AB/@50.9535644,-114.5913106,15z/data=!4m9!4m8!1m0!1m5!1m1!1s0x53711abc8422bf2f:0x165262d28689994d!2m2!1d-114.5692339!2d50.95174!3e0?entry=ttu",
+        date: "July 20, 2021",
+        description: "Not the craziest location for a trip, but it was a small camping/wilderness trip with my close friends. It was all about having fun, exploring the wild and enjoying each others company."
+    }
+
+    
+]
